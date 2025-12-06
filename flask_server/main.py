843f46a6be426a2236578709ed6daf1af69e6fbc
Sheth, Keshav Bhavin <ksheth42@gatech.edu>
2025-12-05 21:31:46 -0500

    Frontend features

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,16 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import mysql.connector
 from flask_cors import CORS
+from datetime import timedelta, datetime
 
 app = Flask(__name__)
 CORS(app)
 
-connection = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '7426',
-    database = 'er_hospital_management'
-)
+def get_connection(): 
+    return mysql.connector.connect(
+        host = 'localhost',
+        user = 'root',
+        password = '7426',
+        database = 'er_hospital_management'
+    )
 
 # @app.route('/getTable', methods=['GET'])
 # def get_table():
@@ -25,25 +27,36 @@ connection = mysql.connector.connect(
 
 @app.route('/views/room_wise_view', methods=['GET'])
 def get_room_wise_view():
+    connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM room_wise_view;")
-    # Saves the result of the executed query into tables 
-    rows = cursor.fetchall()
-    cursor.close()
-    return jsonify(rows), 200
-
-@app.route('/views/symptoms_overview_view', methods=['GET'])
-def get_symptoms_overview_view():
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM symptoms_overview_view;")
     # Saves the result of the executed query into tables 
     rows = cursor.fetchall()
     cursor.close()
     connection.close()
     return jsonify(rows), 200
 
+@app.route('/views/symptoms_overview_view', methods=['GET'])
+def get_symptoms_overview_view():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM symptoms_overview_view;")
+    # Saves the result of the executed query into tables 
+    rows = cursor.fetchall()
+    def convert(o):
+        if isinstance(o, timedelta):
+            return str(o)
+        return o
+
+    rows = [tuple(convert(x) for x in row) for row in rows]
+    cursor.close()
+    connection.close()
+
+    return jsonify(rows), 200
+
 @app.route('/views/medical_staff_view', methods=['GET'])
 def get_medical_staff_view():
+    connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM medical_staff_view;")
     # Saves the result of the executed query into tables 
@@ -54,6 +67,7 @@ def get_medical_staff_view():
 
 @app.route('/views/department_view', methods=['GET'])
 def get_department_view():
+    connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM department_view")
     # Saves the result of the executed query into tables 
@@ -64,6 +78,7 @@ def get_department_view():
 
 @app.route('/views/outstanding_charges_view', methods=['GET'])
 def get_outstanding_charges_view():
+    connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM outstanding_charges_view")
     # Saves the result of the executed query into tables 
@@ -71,6 +86,17 @@ def get_outstanding_charges_view():
     cursor.close()
     connection.close()
     return jsonify(rows), 200
+
+@app.route('/sp/add_patient', methods=['POST'])
+def add_patient():
+    data = request.get_json()
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.callproc("add_patient", (data["ip_ssn"], data["ip_first_name"], data["ip_last_name"], data["ip_birthdate"], data["ip_address"], data["ip_funds"], data["ip_contact"]))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return "OK", 200
 
 if __name__ == '__main__':
     print("connecting")
