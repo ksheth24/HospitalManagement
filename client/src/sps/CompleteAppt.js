d68f7a678f940ceda31faf24d3a35ebe73ebce14
Eshan Jain <50899978+eshanja@users.noreply.github.com>
2025-12-08 14:32:49 -0500

    Add files via upload

---
+++ client/src/sps/CompleteAppt.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+export function CompleteAppt() {
+    const [ssn, setSsn] = React.useState('');
+    const [date, setDate] = React.useState('');
+    const [time, setTime] = React.useState('');
+  
+
+    const submit = async () => {
+        try {
+            const response = await fetch("http://127.0.0.1:5000/sp/add_patient", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    ip_patientId: ssn,
+                    ip_apptDate: date,
+                    ip_apptTime: time
+                })
+            });
+            if (response.ok) {
+                alert("Appointment completed successfully!");
+            }
+        }
+        catch (error) {
+            alert("")
+        }
+    };
+
+    return (
+        <div style = {{display: 'flex', flexDirection: 'column', gap: '1rem', width: '400px'}}>
+            <TextField
+                label = "PatientId"
+                value = {ssn}
+                onChange = {(e) => setSsn(e.target.value)}
+            />
+
+            <TextField
+                label = "Appointment Date"
+                value = {date}
+                onChange = {(e) => setDate(e.target.value)}
+            />
+
+            <TextField
+                label = "Appointment Time"
+                value = {time}
+                onChange = {(e) => setTime(e.target.value)}
+            />
+
+            <Button 
+                variant="outlined"
+                onClick = {submit}>
+                Send
+            </Button>
+
+
+        </div>
+
+        
+    )
+}
